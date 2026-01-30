b94cf66967e36daefaae4236fb3fd56759e89989
Pages <aramkocharyan80@gmail.com>
2026-01-30 13:06:18 +0400

    Just pages

---
+++ app/src/main/java/aram/kocharyan/skillswap/MainActivity.java
@@ -0,0 +1,48 @@
+package aram.kocharyan.skillswap;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+
+        // Listener for bottom navigation
+        bottomNav.setOnItemSelectedListener(item -> {
+
+            Fragment selectedFragment = null;
+            int id = item.getItemId();
+
+            if (id == R.id.nav_schedule) {
+                selectedFragment = new ScheduleFragment();
+
+            } else if (id == R.id.nav_chat) {
+                selectedFragment = new ChatFragment();
+
+            } else if (id == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+
+            } else if (id == R.id.nav_settings) {
+                selectedFragment = new SettingsFragment();
+
+            } else if (id == R.id.nav_profile) {
+                selectedFragment = new ProfileFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container, selectedFragment)
+                        .commit();
+            }
+            return true;
+        });
+    }
+}
